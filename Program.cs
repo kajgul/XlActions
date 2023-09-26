6ae46ab7cb22a4ce1a8bb40fbcd5288425ec6abe
Kajetan Gulbinowicz <kajetan.gulbinowicz@primacoad.local>
2023-09-26 23:18:10 +0200

    Console program which use class library XLActions

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using XlActions;
+
+namespace XLWydruk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xlConnection = new XlConnection();
+            xlConnection.Connection();
+            xlConnection.Login();
+
+            var wydruk = new PrintFromXl();
+            var connection = DbConnection.Connect();
+            var listOfIds = DbGetDocuments.GetDocumentsId(connection);
+            try
+            {
+                foreach (var id in listOfIds)
+                {
+                    wydruk.Print(20220, $"(TrN_GIDTyp=2033 AND TrN_GIDNumer={id})", id);
+                }
+                xlConnection.Logout();
+            }
+            catch(NullReferenceException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.ReadKey();                
+        }
+    }
+}
