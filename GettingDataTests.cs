bfbcc646e8d37fff63907d85e6d8e17b08bd52f5
Kajetan Gulbinowicz <gulbinowicz997@gmail.com>
2023-10-10 20:03:39 +0200

    create XlPrinterUserController.Tests

---
+++ GettingDataTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace XlPrinterUserController.Tests
+{
+    public class GettingDataTests
+    {
+        [Fact]
+        public void GetConnectionData_ValidFilePath_ReturnsDeserializedData()
+        {
+            // Arrange
+            string filePath = "validConnectionData.json";
+            var expectedData = new ConnectionSet(); // Zastąp "YourDataType" odpowiednim typem
+
+            // Tworzenie pliku z danymi
+            File.WriteAllText(filePath, JsonConvert.SerializeObject(expectedData));
+
+            // Act
+            var actualData = GettingData<ConnectionSet>.GetConnectionData(filePath);
+
+            // Assert
+            Assert.Equal(expectedData, actualData);
+        }
+
+        [Fact]
+        public void SaveConnectionData_ValidFilePath_SavesDataToFile()
+        {
+            // Arrange
+            string filePath = "savedConnectionData.json";
+            var dataToSave = new ConnectionSet(); // Zastąp "YourDataType" odpowiednim typem
+
+            // Act
+            GettingData<ConnectionSet>.SaveConnectionData(filePath, dataToSave);
+
+            // Assert
+            Assert.True(File.Exists(filePath));
+            var savedData = File.ReadAllText(filePath);
+            var deserializedData = JsonConvert.DeserializeObject<ConnectionSet>(savedData);
+            Assert.Equal(dataToSave, deserializedData);
+
+            // Clean up
+            File.Delete(filePath);
+        }
+    }
+}
---
+++ SaveSerializedDataTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace XlPrinterUserController.Tests
+{
+    public class SaveSerializedDataTests
+    {
+        [Fact]
+        public void Save_ReturnMessage()
+        {
+            // Arrange
+            string databaseServer = "dbServer";
+            string databaseUser = "dbUser";
+            string passwordUser = "dbPassword";
+            string database = "dbName";
+            string keyServer = "keyServer";
+            string xlUser = "xlUser";
+            string xlPass = "xlPassword";
+            string xlBase = "xlBase";
+            string version = "1";
+
+            // Tworzenie obiektu SaveSerializedData
+            var saveData = new SaveSerializedData(databaseServer, databaseUser, passwordUser, database, keyServer, xlUser, xlPass, xlBase, version);
+
+            // Act
+            string result = saveData.Save();
+
+            // Assert
+            // Sprawdź, czy pliki JSON zostały utworzone
+            string xlPath = @"C:\Users\kajtuch\Desktop\XLLoginInfo.json";
+            string dbPath = @"C:\Users\kajtuch\Desktop\DBLoginInfo.json";
+
+            Assert.True(File.Exists(xlPath));
+            Assert.True(File.Exists(dbPath));
+
+            // Dodatkowo, możesz przeczytać zawartość plików JSON i sprawdzić, czy dane są poprawne
+            // Możesz dodać kod do odczytu plików JSON i sprawdzenia ich zawartości, ale to zależy od Twoich potrzeb.
+
+            // Oczekujemy, że metoda zwróci odpowiedni komunikat
+            Assert.Equal("fasfasfasf", result);
+        }
+    }
+}
